e5faf050d5d4124ec062430eac22c81744fa13e4
AK <balaajay19@gmail.com>
2021-07-04 22:08:22 +0530

    UC

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express';
 
+import resolver from './resolver';
+
+import schema from './schema';
+
+import { graphqlHTTP } from 'express-graphql';
+
 const app = express();
+
+const root = resolver;
 
 app.get('/', (req, res) => {
   res.send('Up and Running ...');
 });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(8082, () => {
   console.log('Running on 8082...');
